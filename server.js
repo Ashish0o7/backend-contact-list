a907b697b68cd53d178b35696c5601190ea0cab0
aashish <75740364+Ashish0o7@users.noreply.github.com>
2023-06-14 20:18:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://ashishkbazad:ashish++7@cluster0.pgfxmdq.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+mongoose.connect(
+    'mongodb+srv://ashishkbazad:ashish++7@cluster0.pgfxmdq.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+);
 
 const contactSchema = new mongoose.Schema({
   name: String,
@@ -18,14 +20,18 @@ const contactSchema = new mongoose.Schema({
   mail2: String,
 });
 
+const expenseSchema = new mongoose.Schema({
+  email: String,
+  item: String,
+  amount: Number,
+});
 
 const Contact = mongoose.model('Contact', contactSchema);
-
+const Expense = mongoose.model('Expense', expenseSchema);
 
 app.post('/contacts', async (req, res) => {
   try {
-    const { name, email, phone,mail2 } = req.body;
-   
+    const { name, email, phone, mail2 } = req.body;
     const newContact = new Contact({ name, email, phone, mail2 });
     const savedContact = await newContact.save();
     res.json(savedContact);
@@ -38,8 +44,7 @@ app.post('/contacts', async (req, res) => {
 app.delete('/contacts/:id', async (req, res) => {
   try {
     const { id } = req.params;
-   
-    await Contact.findOneAndDelete({ _id: id});
+    await Contact.findOneAndDelete({ _id: id });
     res.json({ message: 'Contact deleted successfully' });
   } catch (error) {
     console.log(error);
@@ -49,9 +54,7 @@ app.delete('/contacts/:id', async (req, res) => {
 
 app.get('/contacts', async (req, res) => {
   try {
-   
     const { email } = req.query;
-   
     const contacts = await Contact.find({ email });
     res.json(contacts);
   } catch (error) {
@@ -60,16 +63,15 @@ app.get('/contacts', async (req, res) => {
   }
 });
 
-
 app.put('/contacts/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const { name, phone,mail2 } = req.body;
-    const {email}=req.query;
+    const { name, phone, mail2 } = req.body;
+    const { email } = req.query;
     const updatedContact = await Contact.findOneAndUpdate(
-      { _id: id, email },
-      { name, phone,mail2 },
-      { new: true }
+        { _id: id, email },
+        { name, phone, mail2 },
+        { new: true }
     );
 
     if (!updatedContact) {
@@ -83,9 +85,59 @@ app.put('/contacts/:id', async (req, res) => {
   }
 });
 
+app.post('/expenses', async (req, res) => {
+  try {
+    const { email, item, amount } = req.body;
+    const newExpense = new Expense({ email, item, amount });
+    const savedExpense = await newExpense.save();
+    res.status(201).json(savedExpense);
+  } catch (error) {
+    console.error('Error saving expense:', error);
+    res.status(500).json({ error: 'Failed to save expense' });
+  }
+});
 
+app.get('/expenses', async (req, res) => {
+  try {
+    const { email } = req.query;
+    const expenses = await Expense.find({ email });
+    res.json(expenses);
+  } catch (error) {
+    console.error('Error fetching expenses:', error);
+    res.status(500).json({ error: 'Failed to fetch expenses' });
+  }
+});
+
+app.delete('/expenses/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Expense.findOneAndDelete({ _id: id });
+    res.json({ message: 'Expense deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting expense:', error);
+    res.status(500).json({ error: 'Failed to delete expense' });
+  }
+});
+
+app.put('/expenses/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { item, amount } = req.body;
+    const updatedExpense = await Expense.findByIdAndUpdate(
+        id,
+        { item, amount },
+        { new: true }
+    );
+    if (!updatedExpense) {
+      return res.status(404).json({ error: 'Expense not found' });
+    }
+    res.json(updatedExpense);
+  } catch (error) {
+    console.error('Error updating expense:', error);
+    res.status(500).json({ error: 'Failed to update expense' });
+  }
+});
 
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
 });
-
